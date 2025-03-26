539f1a2ade70e291f22e030c60aa967780bcc6fb
Eman Shawky <emana88shawy@gmail.com>
2025-03-26 23:25:56 +0200

    src folder

---
+++ src/test/java/TestCases/BaseTest.java
@@ -0,0 +1,26 @@
+package TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    public WebDriver baseDriver;
+
+    @BeforeSuite
+    public void beforeClass(){
+        baseDriver = new ChromeDriver();
+        baseDriver.manage().window().maximize();
+        baseDriver.get("https://www.demoblaze.com/");
+
+    }
+
+    @AfterSuite
+    public void afterClass(){
+        if (baseDriver != null) {
+            baseDriver.quit();
+        }
+    }
+}
