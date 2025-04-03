9759b0a21b06896904b48195d5ab59fb6a8c4a2c
Abdullah Abdelbary <91626688+AbdullahAbdelbary@users.noreply.github.com>
2025-04-03 22:06:35 +0200

    Update Main.java
    mb

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,5 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-        System.getProperty("user.dir");
->>>>>>> AbdullahContactFeature
-=======
-
->>>>>>> 4e5c70ef603dcd7959d04f845fb772332aead0f9
     }
 }
